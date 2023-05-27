01c7dd1087c586c70a498409ef08617d71739c2d
Ian Kisali <iankisali@gmail.com>
2023-05-27 18:08:14 +0300

    function obtaining db and retriving rows in user table

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -62,3 +62,21 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     connection = mysql.connector.connect(host=host, database=db_name,
                                          user=username, password=password)
     return connection
+
+
+def main() -> None:
+    """Main function"""
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    for row in cursor:
+        message = f"name={row[0]}; email={row[1]}; phone={row[2]}; " +\
+            f"ssn={row[3]}; password={row[4]};ip={row[5]}; " +\
+            f"last_login={row[6]}; user_agent={row[7]};"
+        print(message)
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    main()
