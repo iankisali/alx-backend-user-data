0e2a2aec0ad8b9394e9951848026970a51a8fefd
Ian Kisali <iankisali@gmail.com>
2023-05-27 18:18:06 +0300

    hash_password function to encrypt passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -2,6 +2,7 @@
 """Encrypting passwords and checking validity"""
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
     """ perform the hashing (with hashpw)."""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
