edc67701ea56332685b7c1ae967d1073d7ce175a
Ian Kisali <iankisali@gmail.com>
2023-05-30 19:23:23 +0300

    Require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -20,6 +20,14 @@ class Auth:
             return False
         return True
 
+        for i in excluded_paths:
+            if i[len(i) - 1] != '*':
+                return False
+            else:
+                if i[:-1] == path[:len(i) - 1]:
+                    return False
+        return True
+
     def authorization_header(self, request=None) -> str:
         """returns None - request will be the Flask request object"""
         if request is None:
