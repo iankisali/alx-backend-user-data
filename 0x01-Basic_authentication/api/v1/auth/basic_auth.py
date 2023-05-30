d0a8e67323dc45704108b80bdc416550a942ffc7
Ian Kisali <iankisali@gmail.com>
2023-05-30 18:59:23 +0300

    Basic - Overload current_user

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -55,3 +55,11 @@ class BasicAuth(Auth):
             if user.is_valid_password(user_pwd):
                 return user
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """retrieves the User instance for a request"""
+        header = self.authorization_header(request)
+        decoded_header = self.extract_base64_authorization_header(header)
+        decoded = self.decode_base64_authorization_header(decoded_header)
+        credentials = self.extract_user_credentials(decoded)
+        return self.user_object_from_credentials(*credentials)
