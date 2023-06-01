f2c8429ac2dc05b86c8b18fafad0c5f5a63031ed
Ian Kisali <iankisali@gmail.com>
2023-06-01 14:02:32 +0300

    session authentication file

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-"""class SessionAuth that inherits from Auth"""
+"""SessionAuth"""
+
 from api.v1.auth.auth import Auth
+from typing import TypeVar
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
-    """Session Auth class with several instances"""
+    """Session auth class"""
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
@@ -18,6 +21,14 @@ class SessionAuth(Auth):
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """returns a User ID based on a Session ID"""
-        if session_id is None or not isinstance(session_id, str):
+        if not session_id or type(session_id) != str:
             return
-        return self.user_id_by_session_id.get(session_id)
+        return SessionAuth.user_id_by_session_id.get(session_id, None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns current_user."""
+        if request:
+            session_cookie = self.session_cookie(request)
+            if session_cookie:
+                user_id = self.user_id_for_session_id(session_cookie)
+                return User.get(user_id)
