0d67deeeaeb1746afab5ed923472815751338277
Ian Kisali <iankisali@gmail.com>
2023-06-07 23:22:29 +0300

    finding user by session id method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -47,3 +47,12 @@ class Auth:
             return session_id
         except NoResultFound:
             return
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """Getting a user session id"""
+        if session_id is None:
+            return
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return
