3c6af99ece39867bd867456060db8b1ed253c3e4
Ian Kisali <iankisali@gmail.com>
2023-06-07 23:26:33 +0300

    adding destroy method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,3 +56,11 @@ class Auth:
             user = self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroying session method"""
+        try:
+            user = self._db.find_user_by(user_id=user_id)
+            self._db.update_user(user.id, session_id=None)
+        except Exception:
+            pass
